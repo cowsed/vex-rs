4d744a31d9e45c73abbf74d24e21a7efb3a53dfb
cowsed <richie.sommers@outlook.com>
2023-12-06 11:04:01 -0500

    no linker errors :) idk if itll actually work tho lol

--- examples/first_robot/build/finish_link.sh
+++
@@ -1,3 +0,0 @@
-name=$(basename $(pwd))
-arm-none-eabi-ld target/armv7a-vex-eabi/release/libfirst_robot.a -T$HOME/.config/Code/User/globalStorage/vexrobotics.vexcode/sdk/cpp/V5/V5_20220726_10_00_00/vexv5/lscript.ld -o target/${name}.elf
-arm-none-eabi-objcopy -O binary target/${name}.elf target/${name}.bin
--- examples/first_robot/build/flash.sh
+++ examples/first_robot/build/flash.sh
@@ -1,2 +1,15 @@
-name=$(basename $(pwd))
-$HOME/.vscode/extensions/vexrobotics.vexcode-0.5.0/resources/tools/vexcom/linux-x64/vexcom --slot $1 --write target/${name}.bin
+#!/usr/bin/env bash
+
+# output of rust compiler
+elf_name=$1
+# binary file we're flashing to the brain
+bin_name="${elf_name%.*}.bin"
+
+# create binary file
+arm-none-eabi-objcopy -O binary ${elf_name} ${bin_name} 
+
+# Show size
+du -h ${bin_name}
+
+# flash the guy
+$HOME/.vscode/extensions/vexrobotics.vexcode-0.5.0/resources/tools/vexcom/linux-x64/vexcom --slot 1 --write ${bin_name}
